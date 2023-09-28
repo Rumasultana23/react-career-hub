4c9cb4462e715acec4e4301a6ab29353508871fb
Rumasultana23 <romaproma23@gmail.com>
2023-09-28 11:24:57 +0600

    added dropDown

---
+++ src/Components/JobDetails/JobDetails.jsx
@@ -0,0 +1,36 @@
+import { useLoaderData, useParams } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { saveJobApplication } from "../../Utility/localStorage";
+
+const JobDetails = () => {
+    const jobs = useLoaderData();
+    const { id } = useParams();
+    const idInt = parseInt(id);
+    const job = jobs.find(job => job.id === idInt);
+    console.log(job);
+
+    const handleApplyJob = () => {
+        saveJobApplication(idInt);
+        toast('You have applied successfully ');
+    }
+    return (
+        <div>
+            
+            <div className="grid gap-4 grid-cols-1 md:grid-cols-4">
+                <div className="border md:col-span-3">
+                    <h2 className="text-4xl">Details coming here</h2>
+                    <h1>Job Details of : {job.job_title}</h1>
+                    <p>{job.company_name}</p>
+                </div>
+                <div className="border ">
+                    <h2 className="text-2xl">Side things</h2>
+                    <button onClick={handleApplyJob} className="btn btn-primary w-full">Apply now</button>
+                </div>
+            </div>
+            <ToastContainer />
+        </div>
+    );
+};
+
+export default JobDetails;
